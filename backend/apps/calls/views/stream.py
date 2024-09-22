eff385ce25cbb19fb40b03a8a4e9fe22e8d11331
itsmackies.design <maruf.tairov@gmail.com>
2024-09-22 06:13:10 +0500

    Change status on call

--- backend/apps/calls/views/stream.py
+++ backend/apps/calls/views/stream.py
@@ -17,7 +17,7 @@ class StartStreamAPIView(APIView):
         call_response = stream_client.video.call("default", f"call_{pk}")
         user_id = f"id_{call.member.id}_{call.member.user.phone}"
         token = stream_client.create_token(user_id)
-        call.status = Call.STATUS_CHOICES[1][1]
+        call.status = Call.STATUS_CHOICES[0][0]
         call_id = call_response.id
         call.call_id = call_id
         call.save()
